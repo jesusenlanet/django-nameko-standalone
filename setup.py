dae629e89240667bee70c0955bd11de4fcb0e53e
Jesús Gutiérrez <jesus.pedro.gutierrez.almazan@gmail.com>
2018-12-09 23:06:10 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
 setuptools.setup(
-    name="django_nameko_standalone",
+    name="django-nameko-standalone",
     version="0.0.1",
     author="Jesus Gutierrez Almazan",
     author_email="jesus.pedro.gutierrez.almazan@gmail.com",
     description="Use django standalone into nameko services",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://gitlab.com/projectsforfun/django-nameko-standalone",
+    url="https://gitlab.com/projectsforfun/django-nameko-standalone/",
     packages=['django_nameko_standalone'],
     install_requires=[
         "Django==2.1.4",
-        "nameko==2.11.0"],
+        "nameko==2.11.0"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
